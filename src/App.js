ee1a5b985cabe4fed0b1268ddaeb21d1d55891bd
Tobi Ogunnaike <tobi@Tobis-MacBook-Pro.local>
2020-12-30 04:12:05 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { PageHeader, Menu, Row, Col } from "antd";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#fbf0e7", minHeight: "100vh" }}>
+      <Header />
     </div>
   );
 }
+
+const Title = () => {};
+
+const Header = () => {
+  // TODO write this in a css file
+  return (
+    <>
+      <div
+        style={{
+          margin: 0,
+          padding: "2vw 4vw",
+          display: "flex",
+          flexWrap: "nowrap",
+          flex: "1 0 67%",
+          alignItems: "center",
+        }}
+      >
+        <h1
+          style={{
+            float: "left", // textAlign?
+            fontSize: `calc((2 - 1) * 1.2vw + 1rem`, // squarespace does this
+            color: "#000",
+            lineHeight: "1.4em",
+            margin: 0,
+            // fontFamily: "aktiv-grotesk",
+          }}
+        >
+          tobi ogunnaike
+        </h1>
+        <div
+          style={{ paddingLeft: "2vw", textAlign: "right", width: "1000px" }}
+        >
+          <InternalLink to="#"> Helloooo</InternalLink>
+          <InternalLink to="#"> Helloooo</InternalLink>
+          <InternalLink to="#"> Helloooo</InternalLink>
+          <InternalLink to="#"> Helloooo</InternalLink>
+          <InternalLink to="#"> Helloooo</InternalLink>
+          {/* <a href="#" target="_blank" rel="noopener">
+            hey
+          </a> */}
+          {/* <span style={{ fontSize: `calc(0vw + 1rem)` }}>hello </span>
+          <span style={{ fontSize: `calc(0vw + 1rem)` }}>hello </span>
+          <span style={{ fontSize: `calc(0vw + 1rem)` }}>hello </span>
+          <span style={{ fontSize: `calc(0vw + 1rem)` }}>hello </span> */}
+        </div>
+      </div>
+      {/* <Menu mode="horizontal">
+          <Menu.Item>Menu</Menu.Item>
+
+          <Menu.Item>SubMenuItem</Menu.Item>
+        </Menu> */}
+    </>
+  );
+};
+
+const InternalLink = ({ to, children }) => {
+  return (
+    <a
+      style={{ color: "#000" }}
+      className="tbd"
+      href={to}
+      target="_blank"
+      rel="noopener"
+    >
+      {children}
+    </a>
+  );
+};
 
 export default App;
